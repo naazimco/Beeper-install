349600fc31e340b2fb2ff6f70535d53d1d06d25e
NaazimCo <naazimco@kwahli.me>
2025-03-20 13:26:46 -0700

    Update Beeper download link to new version

--- Beeper_ChromeOS_install.sh
+++ Beeper_ChromeOS_install.sh
@@ -3,7 +3,7 @@
 echo "Downloading Beeper prerequisites, please enter your password to continue..."
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y nano libnss3 libnotify4 libsecret-1-0 fuse3
-wget -O Beeper.AppImage https://download.beeper.com/linux/appImage/x64
+wget -O Beeper.AppImage https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop
 wget -O beeper.desktop https://raw.githubusercontent.com/naazimco/Beeper-install/main/beeper.desktop
 wget -O icon.png https://raw.githubusercontent.com/naazimco/Beeper-install/main/icon.png
 chmod a+x Beeper.AppImage
