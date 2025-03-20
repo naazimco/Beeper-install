5b5ee0d186efccda31eb8d216136b7bffb0b2f1e
NaazimCo <naazimco@kwahli.me>
2025-03-20 13:31:20 -0700

    Updated to handle new redirection method

--- Beeper_Windows_install.ps1
+++ Beeper_Windows_install.ps1
@@ -1,7 +1,17 @@
-Write-Host "Downloading Beeper for Windows..."
+Write-Host "Fetching latest Beeper download URL..."
 $installerUrl = "https://api.beeper.com/desktop/download/windows/x64/stable/com.automattic.beeper.desktop"
+$finalUrl = (Invoke-WebRequest -Uri $installerUrl -MaximumRedirection 0 -ErrorAction SilentlyContinue).Headers.Location
+
+if (-not $finalUrl) {
+    Write-Host "Error: Failed to retrieve the final download link."
+    exit 1
+}
+
+Write-Host "Downloading Beeper for Windows..."
 $outputPath = "$env:USERPROFILE\Downloads\BeeperSetup.exe"
-Invoke-WebRequest -Uri $installerUrl -OutFile $outputPath -UseBasicParsing
+Invoke-WebRequest -Uri $finalUrl -OutFile $outputPath -UseBasicParsing
+
 Write-Host "Installing Beeper..."
 Start-Process -FilePath $outputPath -ArgumentList "/S" -Wait -Verb RunAs
+
 Write-Host "Beeper installation completed successfully."
