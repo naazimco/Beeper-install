665503342fe87749f22d43057f3e2bf88c67e7b0
NaazimCo <naazimco@kwahli.me>
2025-03-20 13:46:46 -0700

    Refresh script to handle new redirected link

--- Beeper_Linux-Arch_install.sh
+++ Beeper_Linux-Arch_install.sh
@@ -2,8 +2,17 @@
 
 echo "Downloading Beeper prerequisites, please enter your password to continue..."
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm nano nss libnotify libsecret fuse3
-wget -O Beeper.AppImage https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop
+sudo pacman -S --noconfirm nano nss libnotify libsecret fuse
+BEEPER_URL="https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop"
+FINAL_URL=$(curl -Ls -o /dev/null -w %{url_effective} "$BEEPER_URL")
+
+if [[ -z "$FINAL_URL" ]]; then
+    echo "Error: Failed to retrieve the final Beeper download URL."
+    exit 1
+fi
+
+echo "Downloading Beeper from $FINAL_URL..."
+wget -O Beeper.AppImage "$FINAL_URL"
 wget -O beeper.desktop https://raw.githubusercontent.com/naazimco/Beeper-install/main/beeper.desktop
 wget -O icon.png https://raw.githubusercontent.com/naazimco/Beeper-install/main/icon.png
 chmod a+x Beeper.AppImage
