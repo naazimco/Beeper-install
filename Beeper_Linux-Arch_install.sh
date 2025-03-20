2d62fb2a75164c588fd63a897669f977e83dac70
NaazimCo <naazimco@kwahli.me>
2025-03-20 13:26:27 -0700

    Update Beeper download links to new version

--- Beeper_Linux-Arch_install.sh
+++ Beeper_Linux-Arch_install.sh
@@ -3,7 +3,7 @@
 echo "Downloading Beeper prerequisites, please enter your password to continue..."
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm nano nss libnotify libsecret fuse3
-wget -O Beeper.AppImage https://download.beeper.com/linux/appImage/x64
+wget -O Beeper.AppImage https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop
 wget -O beeper.desktop https://raw.githubusercontent.com/naazimco/Beeper-install/main/beeper.desktop
 wget -O icon.png https://raw.githubusercontent.com/naazimco/Beeper-install/main/icon.png
 chmod a+x Beeper.AppImage
